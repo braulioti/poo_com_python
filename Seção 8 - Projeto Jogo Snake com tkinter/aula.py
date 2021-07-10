f072c3027701c9a7d02e4746a455a877b705150c
Bráulio Figueiredo <braulio@braulioti.com.br>
2021-07-10 12:14:12 -0300

    Jogo Snake - Parte 4 (Correções)

--- Seção 8 - Projeto Jogo Snake com tkinter/aula.py
+++ Seção 8 - Projeto Jogo Snake com tkinter/aula.py
@@ -1,7 +1,7 @@
-import random
 import sys
+import random
 from PIL import Image, ImageTk
-from tkinter import Canvas, Frame, NW, ALL, Tk
+from tkinter import Tk, Frame, Canvas, ALL, NW
 
 WIDTH = 300
 HEIGHT = 300
@@ -11,9 +11,9 @@ RAND_POS = 27
 
 class Board(Canvas):
 
-    def __int__(self, parent):
-
-        super().__init__(width=WIDTH, heigth=HEIGHT, background='black', highlightthickness=0)
+    def __init__(self, parent):
+        super().__init__(width= WIDTH, height=HEIGHT,
+                         background='black', highlightthickness=0)
 
         self.parent = parent
         self.ini_game()
@@ -62,6 +62,7 @@ class Board(Canvas):
         overlap = self.find_overlapping(x1, y1, x2, y2)
 
         for ovr in overlap:
+
             if dot[0] == ovr:
                 x, y = self.coords(head)
                 self.create_image(x, y, image=self.body, anchor=NW, tag='body')
@@ -69,7 +70,7 @@ class Board(Canvas):
 
     def locate_dot(self):
 
-        dot = self.find_withtag('dot')
+        dot  = self.find_withtag('dot')
         self.delete(dot[0])
 
         r = random.randint(0, RAND_POS)
@@ -77,7 +78,7 @@ class Board(Canvas):
         r = random.randint(0, RAND_POS)
         self.dot_y = r * DOT_SIZE
 
-        self.create_image(self.dot_x, self.dot_y, image=self.body, anchor=NW, tag='dot')
+        self.create_image(self.dot_x, self.dot_y, image=self.dot, anchor=NW,  tag='dot')
 
     def do_move(self):
 
@@ -87,7 +88,7 @@ class Board(Canvas):
         items = bodys + head
 
         k = 0
-        while(k < len(items) - 1):
+        while( k < len(items) - 1):
             c1 = self.coords(items[k])
             c2 = self.coords(items[k+1])
             self.move(items[k], c2[0]-c1[0], c2[1]-c1[1])
@@ -118,41 +119,41 @@ class Board(Canvas):
                 if body == ovr:
                     self.in_game = False
 
-        if (x1 < 0):
+        if x1 < 0:
             self.in_game = False
 
-        if (x1 < WIDTH - DOT_SIZE):
+        if x1 > WIDTH - DOT_SIZE:
             self.in_game = False
 
-        if (y1 < 0):
+        if y1 < 0:
             self.in_game = False
 
-        if (y1 < HEIGHT - DOT_SIZE):
+        if y1 > HEIGHT - DOT_SIZE:
             self.in_game = False
 
     def on_key_press(self, e):
 
         key = e.keysym
 
-        if (key == 'Left' and not self.right):
+        if key == 'Left' and not self.right:
             self.left = True
             self.up = False
             self.down = False
 
-        if (key == 'Right' and not self.left):
+        if key == 'Right' and not self.left:
             self.right = True
             self.up = False
             self.down = False
 
-        if (key == 'Up' and not self.down):
+        if key == 'Up' and not self.down:
             self.up = True
-            self.right = False
             self.left = False
+            self.right = False
 
-        if (key == 'Down' and not self.up):
+        if key == 'Down' and not self.up:
             self.down = True
-            self.right = False
             self.left = False
+            self.right = False
 
     def on_time(self):
 
@@ -160,12 +161,13 @@ class Board(Canvas):
             self.check_collisions()
             self.check_dot()
             self.do_move()
-            self.after(DELAY, self.on_time())
+            self.after(DELAY, self.on_time)
 
         else:
             self.game_over()
 
     def game_over(self):
+
         self.delete(ALL)
         self.create_text(self.winfo_width()/2, self.winfo_height()/2, text='Game Over', fill='White')
 
@@ -184,3 +186,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
